5c6e3b6f010e0f5bd57a8890c55ef88f2a6ba08b
Omer Ahmed <omerahmed@Mac.telekom.ip>
2025-05-31 01:48:31 +0200

    Prepare the project for Heruko deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rati3)mjrlg1a*+lsk31otl34^p8*q+1d@oy$@u4$3ken)84g&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
